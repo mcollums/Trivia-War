15bb4185c8841835cde94bfe5cba5e29c2831d27
Trinh Nguyen <blossom90vt@hotmail.com>
2019-08-05 13:38:12 -0500

    created basic component files and pages with hamburger menu and some other things

---
+++ client/src/pages/MultiPlayer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Category from "../components/Category";
+
+class MultiPlayer extends Component {
+
+// State goes here
+render(){
+    return (
+        <Category/>
+    )
+}
+}
+
+export default MultiPlayer;
