50bec9d5e1df97c91441cb27813b2b393af6de5b
mcollums <collumsmichelle@gmail.com>
2020-02-01 11:55:22 -0600

    Nav Link styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import API from "../../utils/API.js";
 import { Link } from "react-router-dom";
+import './navStyle.css';
 
 class Nav extends Component {
 
   handleLogout = () => {
     API.logout().catch(err => console.log(err));
-    //TODO Handle Logout for socket.io
   }
 
   render() {
@@ -17,21 +17,22 @@ class Nav extends Component {
         </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+
             <li className="nav-item active" data-toggle="collapse" data-target=".navbar-collapse">
-              {/* <a style={{fontSize: "25px", fontFamily: "'Warnes', cursive"}} className="nav-link" href="/home">Home <span className="sr-only"></span></a> */}
-              <Link to="/home" style={{ fontSize: "25px", fontFamily: "'Warnes', cursive", color: "black", textDecoration: "none" }} className="nav-link">Home</Link>
+              <Link to="/home" className="nav-link primary-link">Home</Link>
             </li>
+            
             <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse">
-              {/* <a className="nav-link" href="/play">Play Now</a> */}
-              <Link to="/play" style={{ color: "black", textDecoration: "none" }}>Play Now</Link>
+              <Link to="/play" className="secondary-link">Play Now</Link>
             </li>
+
             <li className="nav-item">
               <a className="nav-link" href="/" onClick={this.handleLogout}>Logout</a>
             </li>
+
           </ul>
         </div>
-        <Link to="/home" style={{ fontSize: "30px", fontFamily: "'Yatra One', cursive", color: "black", textDecoration: "none" }} className="nav-link" >TRIVIA WAR</Link>
-        {/* <a style={{fontSize: "30px", fontFamily: "'Yatra One', cursive", marginRight:"45%"}} className="navbar-brand" href="/home">TRIVIA WAR</a> */}
+        <Link to="/home" className="nav-link logo-link" >TRIVIA WAR</Link>
       </nav>
     );
   }
