ba7e56757d82217202e035b9712b4fa9f48133df
mcollums <collumsmichelle@gmail.com>
2020-02-13 11:33:42 -0600

    cleaning up server file

--- server.js
+++ server.js
@@ -21,30 +21,30 @@ let io = require('socket.io')(server);
 
 //OAuth
 //============================================================================
-const googleConfig = {
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  redirect: process.env.GOOGLE_REDIRECT_URI
-}
+// const googleConfig = {
+//   clientId: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   redirect: process.env.GOOGLE_REDIRECT_URI
+// }
 
-const defaultScope = [
-  'https://www.googleapis.com/auth/userinfo.email'
-]
+// const defaultScope = [
+//   'https://www.googleapis.com/auth/userinfo.email'
+// ]
 
-function createConnection() {
-  return new google.auth.OAuth2(
-    googleConfig.clientId,
-    googleConfig.clientSecret,
-    googleConfig.redirect
-  )
-}
-function getConnectionUrl() {
-  return createConnection().generateAuthUrl({
-    access_type: 'offline',
-    prompt: 'consent',
-    scope: defaultScope
-  })
-}
+// function createConnection() {
+//   return new google.auth.OAuth2(
+//     googleConfig.clientId,
+//     googleConfig.clientSecret,
+//     googleConfig.redirect
+//   )
+// }
+// function getConnectionUrl() {
+//   return createConnection().generateAuthUrl({
+//     access_type: 'offline',
+//     prompt: 'consent',
+//     scope: defaultScope
+//   })
+// }
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -336,111 +336,111 @@ io.on('connection', function (player) {
 
 // ROUTES FOR GOOGLE AUTHENTICATION
 //=======================================================================
-app.get('/api/google/url', (req, res) => {
-  res.json({ url: getConnectionUrl() })
-})
+// app.get('/api/google/url', (req, res) => {
+//   res.json({ url: getConnectionUrl() })
+// })
 
-function getGoogleAccountFromCode(code) {
-  // console.log("CODE");
-  // console.log(code);
-  return createConnection().getToken(code).then(data => {
-    // console.log("DATA");
-    // console.log(data.tokens)
-    return Promise.resolve(data.tokens)
-  })
-}
+// function getGoogleAccountFromCode(code) {
+//   // console.log("CODE");
+//   // console.log(code);
+//   return createConnection().getToken(code).then(data => {
+//     // console.log("DATA");
+//     // console.log(data.tokens)
+//     return Promise.resolve(data.tokens)
+//   })
+// }
 
-app.post('/api/google/code', (req, res) => {
-  const { code } = req.body;
-  getGoogleAccountFromCode(code).then(tokens => {
-    console.log(tokens)
-    const userConnection = createConnection()
-    userConnection.setCredentials(tokens)
-    userConnection.getTokenInfo(tokens.access_token).then(data => {
-      // console.log("TOKEN INFO");
-      // console.log(data);
-      const { email, sub } = data;
+// app.post('/api/google/code', (req, res) => {
+//   const { code } = req.body;
+//   getGoogleAccountFromCode(code).then(tokens => {
+//     console.log(tokens)
+//     const userConnection = createConnection()
+//     userConnection.setCredentials(tokens)
+//     userConnection.getTokenInfo(tokens.access_token).then(data => {
+//       // console.log("TOKEN INFO");
+//       // console.log(data);
+//       const { email, sub } = data;
 
-      db.User.findOne({ email }).then(dbUser => {
-        if (!dbUser) {
-          // create a new user!
-          db.User.create({
-            email,
-            authType: "google",
-            googleId: sub
-          }).then(finalDbUser => {
-            req.login(finalDbUser, () => {
-              res.json(true)
-            })
-          }).catch(err => {
-            console.log(err)
-            res.sendStatus(500)
-          })
+//       db.User.findOne({ email }).then(dbUser => {
+//         if (!dbUser) {
+//           // create a new user!
+//           db.User.create({
+//             email,
+//             authType: "google",
+//             googleId: sub
+//           }).then(finalDbUser => {
+//             req.login(finalDbUser, () => {
+//               res.json(true)
+//             })
+//           }).catch(err => {
+//             console.log(err)
+//             res.sendStatus(500)
+//           })
 
-        } else {
-          // Check the type and googleId
-          // if it matches, great! Login the user!
-          // if not, something odd is up, reject it
-          // console.log(dbUser);
-          if (dbUser.authType === "google" && dbUser.googleId === sub + "") {
-            req.login(dbUser, () => {
-              res.json(true)
-            });
+//         } else {
+//           // Check the type and googleId
+//           // if it matches, great! Login the user!
+//           // if not, something odd is up, reject it
+//           // console.log(dbUser);
+//           if (dbUser.authType === "google" && dbUser.googleId === sub + "") {
+//             req.login(dbUser, () => {
+//               res.json(true)
+//             });
 
-          } else {
-            res.sendStatus(500)
-          }
-        }
-      })
+//           } else {
+//             res.sendStatus(500)
+//           }
+//         }
+//       })
 
-    }).catch(() => {
-      res.sendStatus(500)
-    })
-  })
-})
+//     }).catch(() => {
+//       res.sendStatus(500)
+//     })
+//   })
+// })
 
-app.get('/api/google/callback', function (req, res) {
-  const code = req.query.code
-  getGoogleAccountFromCode(code).then(tokens => {
-    const userConnection = createConnection()
-    userConnection.setCredentials(tokens)
-    userConnection.getTokenInfo(tokens.access_token).then(data => {
-      const { email, sub } = data;
-      db.User.findOne({ email }).then(dbUser => {
-        // console.log(dbUser);
-        if (!dbUser) {
-          // console.log("NEW USER");
-          // create a new user!
-          db.User.create({
-            email,
-            authType: "google",
-            googleId: sub
-          }).then(finalDbUser => {
-            req.login(finalDbUser, () => {
-              res.redirect(process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/");
-            })
-          }).catch(err => {
-            console.log(err)
-            res.sendStatus(500)
-          })
+// app.get('/api/google/callback', function (req, res) {
+//   const code = req.query.code
+//   getGoogleAccountFromCode(code).then(tokens => {
+//     const userConnection = createConnection()
+//     userConnection.setCredentials(tokens)
+//     userConnection.getTokenInfo(tokens.access_token).then(data => {
+//       const { email, sub } = data;
+//       db.User.findOne({ email }).then(dbUser => {
+//         // console.log(dbUser);
+//         if (!dbUser) {
+//           // console.log("NEW USER");
+//           // create a new user!
+//           db.User.create({
+//             email,
+//             authType: "google",
+//             googleId: sub
+//           }).then(finalDbUser => {
+//             req.login(finalDbUser, () => {
+//               res.redirect(process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/");
+//             })
+//           }).catch(err => {
+//             console.log(err)
+//             res.sendStatus(500)
+//           })
 
-        } else {
-          if (dbUser.authType === "google" && dbUser.googleId === sub + "") {
-            req.login(dbUser, () => {
-              res.redirect(process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/");
-            })
-          } else {
-            res.sendStatus(500)
-          }
-        }
-      }).catch(err => console.log(err))
+//         } else {
+//           if (dbUser.authType === "google" && dbUser.googleId === sub + "") {
+//             req.login(dbUser, () => {
+//               res.redirect(process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/");
+//             })
+//           } else {
+//             res.sendStatus(500)
+//           }
+//         }
+//       }).catch(err => console.log(err))
 
-    }).catch(err => {
-      console.log(err)
-      res.sendStatus(500)
-    })
-  })
-})
+//     }).catch(err => {
+//       console.log(err)
+//       res.sendStatus(500)
+//     })
+//   })
+// })
 
 // Start the API server
 server.listen(PORT, function () {
