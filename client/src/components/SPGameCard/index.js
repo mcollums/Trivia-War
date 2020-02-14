793135f5c743a60de0b4deb37cbadbda9fa62c97
mcollums <collumsmichelle@gmail.com>
2020-02-14 10:28:20 -0600

    spcard container styling and clicks

--- client/src/components/SPGameCard/index.js
+++ client/src/components/SPGameCard/index.js
@@ -3,22 +3,7 @@ import './style.scss';
 
 import { Card } from 'react-bootstrap';
 
-// const styles = {
-//     marginTop: "30px"
-// }
-
 const SPGameCard = props => (
-    <>
-    {/* <div style={styles}
-        id={props.id}
-        className="scategory"
-        onClick={() => props.loadPage(props.id)}>
-        <div>
-            <img className="catImage" src={props.image} alt={props.id} />
-        </div>
-        <div className="scatcat"><strong>{props.category}</strong></div>
-    </div> */}
-
     <Card   id={props.id}
             className="scategory"
             onClick={() => props.loadPage(props.id)}>
@@ -27,8 +12,6 @@ const SPGameCard = props => (
             <Card.Title>{props.category}</Card.Title>
         </Card.Body>
     </Card>
-    </>
-
 );
 
 export default SPGameCard;
