33eed1a8796661fd46ef02b6f0df86c57d403916
mcollums <collumsmichelle@gmail.com>
2020-02-13 21:51:20 -0600

    SPCat Cards styling

--- client/src/pages/SingleCategory.js
+++ client/src/pages/SingleCategory.js
@@ -1,67 +1,55 @@
 import React, { Component } from "react";
 import './Pages.css';
+
+
+import '../styles/Authentication.scss'
+import { Container, Row, Col, Button, Jumbotron } from 'react-bootstrap';
 import API from "../utils/API";
 import SPGameCard from "../components/SPGameCard";
 import SPGameContainer from './SPGameContainer';
 
 
-let addCategory = [];
-
 class SingleCategory extends Component {
     state = {
-        category: [],
+        cat: [],
         id: ""
     };
 
     componentDidMount() {
-        API.getGames().then(res => {
-            // console.log(res.data[0].category);
-            this.getAllGames(res.data);
+        API.getGames()
+            .then(res => {
+                this.setState ({
+                    cat: res.data
+                });
         });
     }
 
     loadPage = (id) => {
-        console.log(id);
-        this.setState({
-            id: id
-        })
+        this.setState({ id: id });
     };
 
-    getAllGames(data) {
-        for (let i = 0; i < data.length; i++) {
-            addCategory.push(data[i]);
-        }
-        this.setState({
-            category: addCategory
-        }, () => {
-            addCategory = [];
-            console.log("State category", this.state.category);
-        });
-    }
 
     render() {
         return (
-            <div>
+            <>
                 {this.state.id === "" ? (
-                    <div className="scatContain">
-                        {this.state.category.map(category => (
+                    <Container className="scatContain">
+                        <Row className="d-flex justify-content-around">
+                        {this.state.cat.map((c, i) => (
                             <SPGameCard
-                                id={category._id}
-                                key={category._id}
-                                category={category.category}
-                                image={category.image}
+                                id={c._id}
+                                key={i}
+                                category={c.category}
+                                image={c.image}
                                 loadPage={this.loadPage}
                             />
                         ))}
-
-                    </div>
-                ) : (
-
-                        <SPGameContainer id={this.state.id} />
-                    )}
-
-
-            </div>
+                        </Row>
+                    </Container>
+                ) 
+                : 
+                (<SPGameContainer id={this.state.id} />)}
+            </>
         )
 
     };
